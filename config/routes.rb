3999a19223268f2106dca225b48c57cbedb247d9
Dominic Villemure <Doumvil@hotmail.com>
2019-03-05 20:04:38 -0500

    Connected pages and added login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+  get 'index' => 'pages#index'
+  get 'RocketElevatorsWebpage_Residential.html' => 'pages#RocketElevatorsWebpage_Residential.html'
+  get 'RocketElevatorsWebpage_Corporate.html' => 'pages#RocketElevatorsWebpage_Corporate.html'
+  get 'RocketElevatorsWebpage_submission.html' => 'pages#RocketElevatorsWebpage_submission.html'
+  get 'login.html' => 'pages#login.html'
+
 end
