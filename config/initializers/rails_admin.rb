be3bc113532303af941749bb8b72c7a03e031ca1
Joanie <Coffey.Joanie@gmail.com>
2019-03-05 22:14:55 -0500

    Database and bux fix

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,9 +1,8 @@
 RailsAdmin.config do |config|
-    config.authenticate_with do
-       warden.authenticate! scope: :admin
-     end
-     config.current_user_method(&:current_admin)
-  end
+  config.authenticate_with do
+     warden.authenticate! scope: :admin
+   end
+   config.current_user_method(&:current_admin)
   ### Popular gems integration
 
   ## == Devise ==
