3f1957d52e030360fd0a26d41296fd6808fff970
Airton <airtonbn@gmail.com>
2019-03-08 17:58:05 -0500

    photo done

--- db/migrate/20190307141733_create_quotes.rb
+++
@@ -1,8 +0,0 @@
-class CreateQuotes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :quotes do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190308130332_add_field_to_create_test_airtons.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldToCreateTestAirtons < ActiveRecord::Migration[5.2]
-  def change
-    add_column :create_test_airtons, :newField, :string
-  end
-end
