0d70d64c0a3c4d04b0007a7aa97e6fedc3a4c92f
Airton <airtonbn@gmail.com>
2019-03-08 10:19:43 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
-
-Rails.application.routes.draw do
-  resources :logins
-  resources :test_airtons
-  resources :dom_quotes
-  resources :quotes
-  devise_for :admin, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  get 'index' => 'pages#index'
-  get 'RocketElevatorsWebpage_Residential.html' => 'pages#RocketElevatorsWebpage_Residential.html'
-  get 'RocketElevatorsWebpage_Corporate.html' => 'pages#RocketElevatorsWebpage_Corporate.html'
-  get 'RocketElevatorsWebpage_submission.html' => 'pages#RocketElevatorsWebpage_submission.html'
-  get 'login.html' => 'pages#login.html'
-
-end
-
 Rails.application.routes.draw do
   devise_for :admin, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
@@ -29,4 +11,3 @@ Rails.application.routes.draw do
   get 'employee' => 'pages#admin/login'
 
 end
-
